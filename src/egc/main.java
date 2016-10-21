8c22fe07310323c8fb35c28fc88d991e9c9c35cb
Jose <josrodtor4@alum.us.es>
2016-10-21 10:01:53 +0200

    primer commit

---
+++ src/egc/main.java
@@ -0,0 +1,23 @@
+package egc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class main {
+
+	
+	@Test
+	public void testResta(){
+		
+		assertEquals(2,sum(2,4));
+	}
+	
+	
+	
+	int sum(int a, int b){
+		
+		return 5;
+	}
+
+}
